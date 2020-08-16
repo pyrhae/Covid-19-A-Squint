7af7a70bfdd35e61098f88fd2cc17455f1df3a0b
pyrhae <pyrha@pm.me>
2020-08-16 18:57:15 +0800

    change theme to sphinx-rtd-theme

--- conf.py
+++ conf.py
@@ -13,9 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import stanford_theme
-html_theme = "stanford_theme"
-html_theme_path = [stanford_theme.get_html_theme_path()]
+
+html_theme = "sphinx_rtd_theme"
+# html_theme_path = [stanford_theme.get_html_theme_path()]
 
 
 # -- Project information -----------------------------------------------------
